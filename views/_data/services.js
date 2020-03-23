468f8ca2cfbb76587d973f1c46f4169f0ea62ac4
Matt Waler <mattwaler@Matts-MBP.local>
2020-03-23 18:05:12 -0400

    some more page polish.

--- views/_data/services.js
+++ views/_data/services.js
@@ -12,6 +12,12 @@ module.exports = [
       'If you need a full-blown traditional content management system, I can build an entirely custom solution using Craft CMS.',
   },
   {
+    image: '/images/backend.jpg',
+    title: 'Deployment',
+    body:
+      'I am an expert at configuring CI/CD tools like Laravel Forge, Netlify, and GitHub Actions to push new code safely and efficiently.',
+  },
+  {
     image: '/images/frontend.jpg',
     title: 'Frontend Development',
     body:
@@ -19,9 +25,9 @@ module.exports = [
   },
   {
     image: '/images/hosting.jpg',
-    title: 'Hosting & Deployment',
+    title: 'Hosting',
     body:
-      'I can configure servers, domains, and continuous integration solutions to make sure your site is lightning-fast and easy to manage.',
+      'Regardless of whether the project is static or dyamic, I can configure servers, domains, and credentials to make sure hosting is dead-simple.',
   },
   {
     image: '/images/consulting.jpg',
