3acc368bb3cd1ab33709184511b9bab06b5e458e
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2020-05-04 11:18:36 -0400

    button font changes and hobby alphabetization.

--- src/views/_data/hobbies.js
+++ src/views/_data/hobbies.js
@@ -1,23 +1,13 @@
 module.exports = [
   {
-    title: 'Hitting the Gym',
-    body:
-      'I try to hit the gym a few times a week to build those vanity muscles. My goal is to look like Brad Pitt in Fight Club.',
-  },
-  {
-    title: 'Watching Movies',
-    body:
-      'I absolutely love movies. My favorite film to date is Jojo Rabbit. Baby Driver is a close second place.',
-  },
-  {
-    title: 'Listening to Podcasts',
-    body:
-      'When doing mundane tasks, I usually toss in my headphones and try to learn from others. I love Syntax and Fullstack Radio.',
-  },
-  {
     title: 'Developing & Improving',
     body:
       'You will often catch me researching, practicing, and trying out the latest and greatest to stay at the top of my game.',
+  },
+  {
+    title: 'Hitting the Gym',
+    body:
+      'I try to hit the gym a few times a week to build those vanity muscles. My goal is to look like Brad Pitt in Fight Club.',
   },
   {
     title: 'Listening to Audiobooks',
@@ -25,8 +15,18 @@ module.exports = [
       'Reading is difficult for me, but I love audiobooks. My favorite listen so far has been Thinking in Bets by Annie Duke.',
   },
   {
+    title: 'Listening to Podcasts',
+    body:
+      'When doing mundane tasks, I usually toss in my headphones and try to learn from others. I love Syntax and Fullstack Radio.',
+  },
+  {
     title: 'Playing Ping-Pong',
     body:
       'I played a lot of Tennis in High School, and since realized that I enjoy the version without running. I love a good game of baby tennis.',
+  },
+  {
+    title: 'Watching Movies',
+    body:
+      'I absolutely love movies. My favorite film to date is Jojo Rabbit. Baby Driver is a close second place.',
   },
 ]
