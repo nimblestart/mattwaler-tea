179b8f348df59214ac31cfb576538b11c9f12ed2
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2020-05-06 16:50:39 -0400

    Adding refs to projects

--- src/views/_data/projects.js
+++ src/views/_data/projects.js
@@ -1,16 +1,16 @@
 module.exports = [
   {
-    title: 'OVERPASS',
+    title: 'Overpass',
     description:
       'Personalized pair-programming sessions to help others learn modern frontend development technologies and tooling.',
     image: '/images/overpass.jpg',
-    link: 'https://overpass.mattwaler.com',
+    link: 'https://overpass.mattwaler.com/?ref=website',
   },
   {
     title: 'Unwind',
     description:
       'A single-page application for relaxation and/or focus. This app automatically saves your selections so you can come back to your favorite sounds.',
     image: '/images/unwind.jpg',
-    link: 'https://unwind.mattwaler.com',
+    link: 'https://unwind.mattwaler.com/?ref=website',
   },
 ]
