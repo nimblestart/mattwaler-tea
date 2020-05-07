dfb7510b848d84495eab6a9a946aeff626ec3430
Matt Waler <32300026+mattwaler@users.noreply.github.com>
2020-05-07 08:33:10 -0400

    Removing refs from projects

--- src/views/_data/projects.js
+++ src/views/_data/projects.js
@@ -4,13 +4,13 @@ module.exports = [
     description:
       'Personalized pair-programming sessions to help others learn modern frontend development technologies and tooling.',
     image: '/images/overpass.jpg',
-    link: 'https://overpass.mattwaler.com/?ref=website',
+    link: 'https://overpass.mattwaler.com',
   },
   {
     title: 'Unwind',
     description:
       'A single-page application for relaxation and/or focus. This app automatically saves your selections so you can come back to your favorite sounds.',
     image: '/images/unwind.jpg',
-    link: 'https://unwind.mattwaler.com/?ref=website',
+    link: 'https://unwind.mattwaler.com',
   },
 ]
