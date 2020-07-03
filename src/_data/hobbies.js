9c415db9bffe2f3fb426ab8e35a29abd48813d56
Matt Waler <mattwaler@Matt-Waler-MBP-16.local>
2020-07-03 11:41:17 -0400

    Pulling content out of markup and into data files

--- src/_data/hobbies.js
+++ src/_data/hobbies.js
@@ -1,32 +1,37 @@
-module.exports = [
-  {
-    title: 'Hitting the Gym',
-    body:
-      'I try to hit the gym a few times a week to build those vanity muscles. My goal is to look like Brad Pitt in Fight Club.',
-  },
-  {
-    title: 'Listening to Audiobooks',
-    body:
-      'Reading is difficult for me, but I love audiobooks. My favorite listen so far has been Thinking in Bets by Annie Duke.',
-  },
-  {
-    title: 'Listening to Podcasts',
-    body:
-      'When doing mundane tasks, I usually toss in my headphones and try to learn from others. I love Syntax and Fullstack Radio.',
-  },
-  {
-    title: 'Playing Tennis',
-    body:
-      'I played a lot of Tennis in High School, and have since rekindled the flame. Currently working on getting my topspin back to what it used to be.',
-  },
-  {
-    title: 'Watching Movies',
-    body:
-      'I absolutely love movies. I am a sucker for anything by Edgar Wright. My favorite film to date is Jojo Rabbit by Taika Waititi.',
-  },
-  {
-    title: 'Web Development',
-    body:
-      'You will often catch me researching, practicing, and trying out the latest and greatest to stay at the top of my game.',
-  },
-]
+module.exports = {
+  heading: 'A little bit about me.',
+  subheading:
+    "I'm an Ohio University graduate from Broadview Heights, Ohio. I love development, lifting, and the occasional blockbuster. I like to think I'm decently funny, but my friends can assure you that I am not.",
+  list: [
+    {
+      title: 'Hitting the Gym',
+      body:
+        'I try to hit the gym a few times a week to build those vanity muscles. My goal is to look like Brad Pitt in Fight Club.',
+    },
+    {
+      title: 'Listening to Audiobooks',
+      body:
+        'Reading is difficult for me, but I love audiobooks. My favorite listen so far has been Thinking in Bets by Annie Duke.',
+    },
+    {
+      title: 'Listening to Podcasts',
+      body:
+        'When doing mundane tasks, I usually toss in my headphones and try to learn from others. I love Syntax and Fullstack Radio.',
+    },
+    {
+      title: 'Playing Tennis',
+      body:
+        'I played a lot of Tennis in High School, and have since rekindled the flame. Currently working on getting my topspin back to what it used to be.',
+    },
+    {
+      title: 'Watching Movies',
+      body:
+        'I absolutely love movies. I am a sucker for anything by Edgar Wright. My favorite film to date is Jojo Rabbit by Taika Waititi.',
+    },
+    {
+      title: 'Web Development',
+      body:
+        'You will often catch me researching, practicing, and trying out the latest and greatest to stay at the top of my game.',
+    },
+  ],
+}
--- src/_data/projects.js
+++ src/_data/projects.js
@@ -1,16 +1,21 @@
-module.exports = [
-  {
-    title: 'Overpass',
-    description:
-      'Personalized pair-programming sessions to help others learn modern frontend development technologies and tooling.',
-    image: '/images/overpass.jpg',
-    link: 'https://overpass.mattwaler.com',
-  },
-  {
-    title: 'Unwind',
-    description:
-      'A single-page application for relaxation and/or focus. This app automatically saves your selections so you can come back to your favorite sounds.',
-    image: '/images/unwind.jpg',
-    link: 'https://unwind.mattwaler.com',
-  },
-]
+module.exports = {
+  heading: 'Stuff I am working on.',
+  subheading:
+    'A few pet projects that I spend my spare development time on. Check them out or view the source code on GitHub.',
+  list: [
+    {
+      title: 'Overpass',
+      description:
+        'Personalized pair-programming sessions to help others learn modern frontend development technologies and tooling.',
+      image: '/images/overpass.jpg',
+      link: 'https://overpass.mattwaler.com',
+    },
+    {
+      title: 'Unwind',
+      description:
+        'A single-page application for relaxation and/or focus. This app automatically saves your selections so you can come back to your favorite sounds.',
+      image: '/images/unwind.jpg',
+      link: 'https://unwind.mattwaler.com',
+    },
+  ],
+}
