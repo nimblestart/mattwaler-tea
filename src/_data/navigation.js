3fd0a5f7f33543e55f871edc20bebc27fdefda06
Matt Waler <mattwaler@Matt-Waler-MBP-16.local>
2020-08-25 23:19:25 -0400

    More tweaks to header and footer

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,10 +1,10 @@
 module.exports = {
-  header: [
+  internal: [
     { name: 'About', link: '/about/' },
     { name: 'Blog', link: '/blog/' },
     { name: 'Contact', link: '/contact/' },
   ],
-  footer: [
+  external: [
     { icon: 'github', link: 'https://github.com/mattwaler' },
     { icon: 'linkedin', link: 'https://www.linkedin.com/in/mattwaler/' },
     { icon: 'dev', link: 'https://dev.to/mattwaler' },
