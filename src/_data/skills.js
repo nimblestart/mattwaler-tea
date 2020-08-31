6512ba24f40089fe0e347bdeea4f65111e9c002f
Matt Waler <mwaler@users.noreply.github.com>
2020-08-31 14:18:56 -0400

    Adjusting design content per Spencer's feedback.

--- src/_data/skills.js
+++ src/_data/skills.js
@@ -20,7 +20,7 @@ module.exports = [
   {
     name: 'Design',
     description:
-      "While not my focus, I am perfectly capable of coming up with visually pleasing wireframes and designs.",
+      "I can whip up both wireframes and high-fidelity designs in Sketch, Figma, or whatever design tool you prefer.",
     icon: 'design',
     color: 'text-blue-500',
     link: '/sites/',
